07da0c5d499b20413ad0ccef2cf2c36afa8c3384
baokowjbu-oss <baokowjbu@gmail.com>
2025-11-01 02:44:21 +0700

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
-import { MenteeSessions } from "./pages/MenteeSessions";
+import { RoleSelectionPage } from "./pages/RoleSelectionPage";
+import { MenteeHomePage } from "./pages/MenteeHomePage";
+import { MenteeRegistrationPage } from "./pages/MenteeRegistrationPage";
+import { MenteeSchedulePage } from "./pages/MenteeSchedulePage";
 import { TutorSessions } from "./pages/TutorSessions";
 import { CoordinatorDashboard } from "./pages/CoordinatorDashboard";
+import { FeedbackPage } from "./pages/FeedbackPage";
+import { MenteeProgressPage } from "./pages/MenteeProgressPage";
+import { PrivateNotePage } from "./pages/PrivateNotePage";
+import { ProvideAssignmentPage } from "./pages/ProvideAssignmentPage";
+import { HomeworkPage } from "./pages/HomeworkPage";
+import { QuizPage } from "./pages/QuizPage";
+import { MenteeAssignmentsPage } from "./pages/MenteeAssignmentsPage";
+import { MenteeQuizAttemptPage } from "./pages/MenteeQuizAttemptPage";
+import { MySessionsPage } from "./pages/MySessionsPage";
+import { ProgressTrackingPage } from "./pages/ProgressTrackingPage";
 import { CoordinatorProfile } from "./pages/CoordinatorProfile";
 import { MenteeProfile } from "./pages/MenteeProfile";
 import { TutorProfile } from "./pages/TutorProfile";
+import Card from "./pages/SessionManager/Card";
+
 
 function App() {
   return (
@@ -14,12 +29,33 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/sessions" element={<MenteeSessions />} />
+        <Route path="/role-selection" element={<RoleSelectionPage />} />
+        
+        {/* Mentee Routes */}
+        <Route path="/mentee/home" element={<MenteeHomePage />} />
+        <Route path="/mentee/registration" element={<MenteeRegistrationPage />} />
+        <Route path="/mentee/schedule" element={<MenteeSchedulePage />} />
+        {/* <Route path="/sessions" element={<MenteeSessions />} /> */}
+        <Route path="/mentee/sessions" element={<MySessionsPage />} />
+        <Route path="/mentee/sessions/feedback/:id" element={<FeedbackPage />} />
+        <Route path="/mentee/sessions/assignments" element={<MenteeAssignmentsPage />} />
+        <Route path="/mentee/sessions/quiz/:id" element={<MenteeQuizAttemptPage />} />
+        <Route path="/mentee/profile" element={<MenteeProfile />} />
+        
+        {/* Tutor Routes */}
         <Route path="/tutor/sessions" element={<TutorSessions />} />
+        <Route path="/tutor/mark_attendance/:classId" element={<Card />} />
+        <Route path="/assignment" element={<ProvideAssignmentPage />} />
+        <Route path="/assignment/homework/:id" element={<HomeworkPage />} />
+        <Route path="/assignment/quiz/:id" element={<QuizPage />} />
+        <Route path="/tutor/mentees_list" element={<ProgressTrackingPage />} />
+        <Route path="/mentee_progress/:id" element={<MenteeProgressPage />} />
+        <Route path="/mentee_progress/:id/private_note" element={<PrivateNotePage />} />
+        <Route path="/tutor/profile" element={<TutorProfile />} />
+        
+        {/* Coordinator Routes */}
         <Route path="/coordinator/dashboard" element={<CoordinatorDashboard />} />
         <Route path="/coordinator/profile" element={<CoordinatorProfile />} />
-        <Route path="/mentee/profile" element={<MenteeProfile />} />
-        <Route path="/tutor/profile" element={<TutorProfile />} />
       </Routes>
     </BrowserRouter>
   );
